510f7024c9ec009dd626f3f54de884a301f07e7b
rfcho322 <98252280+rfcho322@users.noreply.github.com>
2023-01-24 12:44:04 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener('click', () => {
+    primaryNav.hasAttribute("data-visible") 
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true)
+    primaryNav.toggleAttribute("data-visible");
+    primaryHeader.toggleAttribute("data-overlay");
+});
+
+
+/** SLIDER JS **/
+const slider = new A11YSlider(document.querySelector('.slider'), {
+    adaptiveHeight: false,
+    dots: true,
+    centerMode: true,
+    arrows: false,
+    responsive: {
+        480: {
+          dots: false, //dots enabled 480px and up
+        }
+    }
+});
